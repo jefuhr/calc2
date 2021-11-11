c903c322cbd08e877db6eff568927b15b3120915
Jacob <jefuhr98@gmail.com>
2021-11-11 15:23:42 -0500

    part 2

---
+++ calculator/calculations/multiplication.py
@@ -0,0 +1,10 @@
+from calc2.calculations.calculation import Calculation
+
+class Multiplication(Calculation):
+
+    def get_result(self):
+
+        result = 1.0
+        for value in self.values:
+            result = result * value
+        return result
--- calculator/main.py
+++ calculator/main.py
@@ -1,21 +1,29 @@
-""" This is the increment function"""
-class Calculator:
+from calculator.history.calculations import Calculations
+
+#the calculator class just contains the methods to calculate
+class Main:
     """ This is the Calculator class"""
+    #the calculator class just calls methods on Calculations class
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
 
-    def add_number(self, value_a):
-        """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
-        """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
-        """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
+    @staticmethod
+    def get_last_result_value():
+        """ This is the gets the result of the calculation"""
+        # I made this method so that I don't have more than one action per function
+        return Calculations.get_last_calculation_result_value()
+    @staticmethod
+    #tuple allows me to pass in as many values as a I want
+    def add_numbers(tuple_values: tuple):
+        """ adds list of numbers"""
+        Calculations.add_addition_calculation(tuple_values)
+        return True
+    @staticmethod
+    def subtract_numbers(tuple_values: tuple):
+        """ subtract a list of numbers from result"""
+        Calculations.add_subtraction_calculation(tuple_values)
+        return True
+    @staticmethod
+    def multiply_numbers(tuple_values: tuple):
+        """ multiplication number from result"""
+        Calculations.add_multiplication_calculation(tuple_values)
+        return True
